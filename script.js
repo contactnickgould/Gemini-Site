751a505a593146fa653b54185727029e3550d188
Nick Gould <contact.nickgould@gmail.com>
2025-02-18 08:06:21 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'none'; // Hide preloader when page loads
+});
+
+// Add JavaScript for other interactive elements
